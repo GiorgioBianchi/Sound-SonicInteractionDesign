cd0aff14940540676421605232cb7530138170df
GiorgioBianchi <63044847+GiorgioBianchi@users.noreply.github.com>
2022-10-22 15:40:49 +0200

    bug fix fade images

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,13 @@
 const IMAGE1_TOP = 1,
 IMAGE1_BOTTOM = 17.2, 
 IMAGE2_TOP = 24.6, 
-IMAGE_1_2_DIFF = IMAGE2_TOP - IMAGE1_BOTTOM,
+IMAGE_DIFF = IMAGE2_TOP - IMAGE1_BOTTOM,
 IMAGE2_BOTTOM = 39.5, 
 IMAGE3_TOP = 47.0,
-IMAGE_2_3_DIFF = IMAGE3_TOP - IMAGE2_BOTTOM,
 IMAGE3_BOTTOM = 57.79,
 IMAGE4_TOP = 65.3,
-IMAGE_3_4_DIFF = IMAGE4_TOP - IMAGE3_BOTTOM,
 IMAGE4_BOTTOM = 77.27,
 IMAGE5_TOP = 84.92,
-IMAGE_4_5_DIFF = IMAGE5_TOP - IMAGE4_BOTTOM,
 IMAGE5_BOTTOM = 100;
 
 let context,
@@ -39,7 +36,7 @@ $( document ).ready(function() {
             if(sound_2Img.playing()) sound_2Img.volume(0);
         } else if(scrollPercent > IMAGE1_BOTTOM && scrollPercent < IMAGE2_TOP) {
             soundFade = scrollPercent - IMAGE1_BOTTOM;
-            volume = (IMAGE_1_2_DIFF / 100) * soundFade;
+            volume = (soundFade/IMAGE_DIFF);
             sound_2Img.volume(volume);
             sound_1Img.volume(1 - volume);
         } else if( scrollPercent > IMAGE2_TOP && scrollPercent < IMAGE2_BOTTOM) {
@@ -48,16 +45,16 @@ $( document ).ready(function() {
             if(sound_3Img.playing()) sound_3Img.volume(0);
         } else if( scrollPercent > IMAGE2_BOTTOM && scrollPercent < IMAGE3_TOP) {
             soundFade = scrollPercent - IMAGE2_BOTTOM;
-            volume = (IMAGE_2_3_DIFF / 100) * soundFade;
+            volume = (soundFade/IMAGE_DIFF);
             sound_3Img.volume(volume);
-            sound_2Img.volume(1 - volume);
+            sound_2Img.volume(1.0 - volume);
         } else if( scrollPercent > IMAGE3_TOP && scrollPercent < IMAGE3_BOTTOM) {
             if(sound_2Img.playing()) sound_2Img.volume(0);
             sound_3Img.volume(1);
             if(sound_4Img.playing()) sound_4Img.volume(0);
         } else if( scrollPercent > IMAGE3_BOTTOM && scrollPercent < IMAGE4_TOP) {
             soundFade = scrollPercent - IMAGE3_BOTTOM;
-            volume = (IMAGE_3_4_DIFF / 100) * soundFade;
+            volume = (soundFade/IMAGE_DIFF);
             sound_4Img.volume(volume);
             sound_3Img.volume(1 - volume);
         } else if( scrollPercent > IMAGE4_TOP && scrollPercent < IMAGE4_BOTTOM) {
@@ -66,7 +63,7 @@ $( document ).ready(function() {
             if(sound_5Img.playing()) sound_5Img.volume(0);
         } else if( scrollPercent > IMAGE4_BOTTOM && scrollPercent < IMAGE5_TOP) {
             soundFade = scrollPercent - IMAGE4_BOTTOM;
-            volume = (IMAGE_4_5_DIFF / 100) * soundFade;
+            volume = (soundFade/IMAGE_DIFF);
             sound_5Img.volume(volume);
             sound_4Img.volume(1 - volume);
         } else if( scrollPercent > IMAGE5_TOP && scrollPercent < IMAGE5_BOTTOM) {
@@ -119,7 +116,7 @@ function startExperience() {
             src: ['./assets/sounds/tracks/5.mp3'],
             loop: true,
         });
-        
+
         console.log('PLAY');
         sound_1Img.fade(0.0, 1.0, 500);
         sound_1Img.play();
